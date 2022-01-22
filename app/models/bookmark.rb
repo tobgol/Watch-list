b464bf7b44a90739804353d593157b73e51a7d4b
tobiasz golian <tobiasz.golian@travelperk.com>
2022-01-22 12:08:18 +0100

    models

---
+++ app/models/bookmark.rb
@@ -0,0 +1,7 @@
+class Bookmark < ApplicationRecord
+  belongs_to :list
+  belongs_to :movie
+
+  validates :comment, length: { minimum: 6}
+  validates_uniqueness_of :movie_id, scope: [:list_id]
+end
