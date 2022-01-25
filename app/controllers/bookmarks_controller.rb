a433d9cb9bb587c24823bafcb39c1bc515e4393b
tobiasz golian <tobiasz.golian@travelperk.com>
2022-01-25 19:10:57 +0100

    new bundle installed

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,38 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: [ :index, :new, :create, :destroy ]
+
+  def index
+    @bookmarks = Bookmark.where(list_id: @list)
+  end
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+end
